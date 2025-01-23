7b2e24c95fe300de9212548123ebbb764ed2ef8f
samuel.documet <samuel.documet@pagonxt.com>
2025-01-23 02:09:01 +0100

    core(FRONT-0003): add angular material to shell host

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, MatSlideToggleModule, MatCardModule, MatRadioModule, FormsModule, MatCheckboxModule],
   selector: 'shell-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'shell';
+  checked=false;
+  disabled=false;
 }
