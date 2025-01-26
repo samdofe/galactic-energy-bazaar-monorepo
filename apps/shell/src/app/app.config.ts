60c0a283580e3806910e58c9734ac04e6045c6f0
samuel.documet <samuel.documet@pagonxt.com>
2025-01-26 02:27:42 +0100

    feat(FRONT-0008): bug fixes

--- apps/shell/src/app/app.config.ts
+++ apps/shell/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { fedsCoreAuthInterceptor } from '@feds/core/auth';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideNoopAnimations(),
     provideHttpClient(withInterceptors([fedsCoreAuthInterceptor])),
   ],
 };
